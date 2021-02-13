83026496a4818b4d69953fdeb13cb0f3cf5023e3
Sadmachine <sansuu716@gmail.com>
2021-02-13 14:40:19 +0900

    画像ファイル等の追加

--- smash.meme/smash.meme/ContentView.swift
+++ smash.meme/smash.meme/ContentView.swift
@@ -1,9 +1,5 @@
-//
-//  ContentView.swift
-//  smash.meme
-//
-//  Created by K K on 2021/02/13.
-//
+//smash. のUIを再現するアプリケーションのα版
+//開発者の画像を例に表示するよ！
 
 import SwiftUI
 
@@ -11,14 +7,84 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image("DSC02475")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 5000, height: 1500)
+                .offset(x: -25, y: 250)
+            Image("image")
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 5000, height: 1500)
-                .offset(x: -30, y: 280)
-            }
+                .frame(width: 5, height: 15)
         }
-        
+        VStack{
+            Sheekbar()
+            Images()
+            Share()
+            Setting()
+            Stopgo()
+            Uparrow()
+            
+            
+                
+        }
+        }
     }
+
+//以下，UIボタンなど汎用的に使うもののビューの定義
+
+struct Setting : View{
+    var body: some View {
+        Image("setting")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+    }
+}
+
+struct Images : View{
+    var body: some View {
+        Image("image")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+    }
+}
+
+struct Share : View{
+    var body: some View {
+        Image("share")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+    }
+}
+
+struct Sheekbar : View{
+    var body: some View {
+        Image("sheekbar")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 400, height: 100)
+    }
+}
+
+struct Stopgo : View{
+    var body: some View {
+        Image("stopgo")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 350, height: 100)
+    }
+}
+
+struct Uparrow : View{
+    var body: some View {
+        Image("uparrow")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 50, height: 50)
+    }
+}
+
 
 
 struct ContentView_Previews: PreviewProvider {
